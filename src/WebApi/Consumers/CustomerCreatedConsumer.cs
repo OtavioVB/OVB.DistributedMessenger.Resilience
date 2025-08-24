adcfaaaf36c9bd76d54194b9fee4dc8de7cb800d
Otavio Carmanini <otaviovb.developer@gmail.com>
2025-08-24 14:41:36 -0300

    feature: add dlq consumer and retries

--- src/WebApi/Consumers/CustomerCreatedConsumer.cs
+++ src/WebApi/Consumers/CustomerCreatedConsumer.cs
@@ -44,8 +44,6 @@ public sealed class CustomerCreatedConsumer : BackgroundService
 
             while (!stoppingToken.IsCancellationRequested) 
             {
-                var scope = _serviceProvider.CreateScope();
-
                 var @event = consumer.Consume(stoppingToken);
                 
                 await ExecuteHandleAsync(@event, stoppingToken);
@@ -55,16 +53,79 @@ public sealed class CustomerCreatedConsumer : BackgroundService
             consumer.Dispose();
         }, stoppingToken);
 
+        var consumerDlq = new ConsumerBuilder<string, string>(consumerConfig).Build();
+
+        consumerDlq.Subscribe(TOPIC_NAME_DLQ);
+
+        _ = Task.Run(async () =>
+        {
+            _logger.LogInformation("[{Type}][{Method}] The apache kafka dlq consumer has been started. Info = {@Info}",
+                nameof(CustomerCreatedConsumer),
+                nameof(ExecuteAsync),
+                new
+                {
+                    EventType = nameof(CustomerCreatedConsumer),
+                    GroupId = consumerConfig.GroupId,
+                    TopicName = TOPIC_NAME,
+                });
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var @event = consumerDlq.Consume(stoppingToken);
+
+                await ExecuteHandleAsync(@event, stoppingToken);
+            }
+
+            consumerDlq.Close();
+            consumerDlq.Dispose();
+        });
+
         return Task.CompletedTask;
+    }
+
+    public async Task ExecuteHandlingToDlqAsync(ConsumeResult<string, string> @event, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await ExecuteHandleAsync(@event, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                exception: ex,
+                "[{Type}][{Method}] Message has been received and sent to DLQ queue. Message = {@Message}",
+                nameof(CustomerCreatedConsumer),
+                nameof(ExecuteHandlingToDlqAsync),
+                JsonSerializer.Serialize(@event));
+
+            var producerConfig = _serviceProvider.GetRequiredService<ProducerConfig>();
+
+            var producer = new ProducerBuilder<string, string>(producerConfig).Build();
+
+            var message = new Message<string, string>()
+            {
+                Key = @event.Message.Key, 
+                Value = @event.Message.Value
+            };
+
+            await producer.ProduceAsync(
+                topic: TOPIC_NAME_DLQ,
+                message: message,
+                cancellationToken: cancellationToken);
+        }
     }
 
     public async Task ExecuteHandleAsync(ConsumeResult<string, string> @event, CancellationToken cancellationToken = default)
     {
         var message = JsonSerializer.Deserialize<Customer>(@event.Message.Value);
 
+        throw new Exception("error");
+
         _logger.LogInformation("[{Type}][{Method}] Message has been received and processed. Message = {@Message}",
             nameof(CustomerCreatedConsumer),
             nameof(ExecuteHandleAsync),
             JsonSerializer.Serialize(@event));
+
+        await Task.Delay(100, cancellationToken);
     }
 }
